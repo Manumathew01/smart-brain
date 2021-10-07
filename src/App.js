e31eb9fcc0713d32918306e452e97e0b3cc7139b
Manumathew01 <manu.thoppil01@gmail.com>
2021-10-07 21:40:49 +0530

    fixed bounding box issue

--- src/App.js
+++ src/App.js
@@ -1,58 +1,80 @@
-import React, { Component } from 'react';
-import Clarifai from 'clarifai';
-import ParticleBackground from 'react-particle-backgrounds';
-import FaceRecognition from './components/FaceRecognition/FaceRecognition';
-import Navigation from './components/Navigation/Navigation';
-import Logo from './components/Logo/Logo';
-import Rank from './components/Rank/Rank';
-import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
-import particleSettings from './particleSettings';
-import './App.css';
+import React, { Component } from "react";
+import Clarifai from "clarifai";
+import ParticleBackground from "react-particle-backgrounds";
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
+import Navigation from "./components/Navigation/Navigation";
+import Logo from "./components/Logo/Logo";
+import Rank from "./components/Rank/Rank";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import particleSettings from "./particleSettings";
+import "./App.css";
 
 const app = new Clarifai.App({
-  apiKey: 'a77a26db39fd414eb3e462e4400710a3'
- });
-
+  apiKey: "a77a26db39fd414eb3e462e4400710a3",
+});
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
-      input: '',
-      imageUrl: ''
-    }
+      input: "",
+      imageUrl: "",
+      box: {},
+    };
   }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace =
+      data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById("inputimage");
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height,
+    };
+  };
+
+  displayFaceBox = (box) => {
+    this.setState({ box: box });
+  };
 
   onInputChange = (event) => {
-   this.setState({input: event.target.value});
-  
-  }
+    this.setState({ input: event.target.value });
+  };
   onButtonSubmit = () => {
-    this.setState({imageUrl: this.state.input});
+    this.setState({ imageUrl: this.state.input });
     app.models
-      .predict( Clarifai.FACE_DETECT_MODEL,this.state.input)
-      .then(response => {
-        console.log(response.outputs[0].data.regions[0].region_info.bounding_box)
+      .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+      .then((response) => {
+        this.displayFaceBox(this.calculateFaceLocation(response));
+        console.log(
+          response.outputs[0].data.regions[0].region_info.bounding_box
+        );
+        console.log(response.outputs[0].data.regions); //multiple faces
       })
-      .catch(err => {
-        console.log(err)
-      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-  }
-  
   render() {
     return (
-      <div className = "App">
-        <ParticleBackground className = 'bubbles' settings ={particleSettings}/>
+      <div className="App">
+        <ParticleBackground className="bubbles" settings={particleSettings} />
         <Navigation />
-        <Logo /> 
-        <Rank />
-        <ImageLinkForm onInputChange ={this.onInputChange} onButtonSubmit ={this.onButtonSubmit}  />
-        <FaceRecognition imageUrl={this.state.imageUrl}/>
+        <Logo />
+        {/* <Rank /> */}
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
       </div>
-     );
+    );
   }
-
 }
 
 export default App;
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,22 +1,29 @@
-import React from 'react';
-import './ImageLinkForm.css';
-
+import React from "react";
+import "./ImageLinkForm.css";
 
 const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
-  return(
-  <div className = 'f3'>
-    <p className = 'white'>
-      {'Detect faces in images'}
-    </p>
-    <div className = 'center'>
-      <div className = 'form-pattern center pa4 br3 shadow-4 ' >
-        <input className ='f4 pa2 w-70 center' type ='text' onChange = {onInputChange} />
-        <button className = 'w-30 link f4 ph3 pv2 dib white button' onClick = {onButtonSubmit} >Detect</button>
+  return (
+    <div className="f3">
+      <h2 style={{ letterSpacing: "2px" }} className="f2 white">
+        {"Detect faces in images!!"}
+      </h2>
+      <div className="center">
+        <div className="form-pattern center pa4 br3 shadow-4 ">
+          <input
+            className="f4 pa2 w-70 center"
+            type="text"
+            onChange={onInputChange}
+          />
+          <button
+            className="w-30 link f4 ph3 pv2 dib white button"
+            onClick={onButtonSubmit}
+          >
+            Detect
+          </button>
+        </div>
       </div>
     </div>
-    </div>
-  
   );
-}
+};
 
 export default ImageLinkForm;
