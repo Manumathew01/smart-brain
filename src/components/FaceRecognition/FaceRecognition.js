45017496ba9391f7880bee82d4f32db154168b17
Manu Mathew <56392857+Manumathew01@users.noreply.github.com>
2021-10-08 00:50:10 +0530

    Update FaceRecognition.js

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -4,7 +4,6 @@ import "./FaceRecognition.css";
 const FaceRecognition = ({ imageUrl, boxes }) => {
   return (
     <div className="center ma">
-<<<<<<< HEAD
       <div className="absolute mt5">
         <img
           id="inputImage"
@@ -27,19 +26,6 @@ const FaceRecognition = ({ imageUrl, boxes }) => {
             ></div>
           );
         })}
-=======
-      <div className="absolute mt2">
-        <img id="inputimage" alt="" src={imageUrl} width="500px" height="auto" />
-        <div
-          className="bounding-box"
-          style={{
-            top: box.topRow,
-            right: box.rightCol,
-            bottom: box.bottomRow,
-            left: box.leftCol,
-          }}
-        ></div>
->>>>>>> origin/main
       </div>
       {boxes.length !== 0 ? (
         <div className=" f3 mb5 white">
